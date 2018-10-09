5db1767cfa5faacc6046dd129124e78acd507b77
Anthony <anthonyduguid@gmail.com>
2018-10-09 11:02:12 +1100

    Delete badpath.bat

--- badpath.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-rem  This batch file will mess up your command prompt's PATH setting.  Without
-rem  setlocal and endlocal, set-ting variables changes the DOS environment.
-
-set path=abc
