a6efec53d8f0a4a411a94066076c3a80e2efdeb3
Anthony <anthonyduguid@gmail.com>
2018-10-09 11:05:51 +1100

    Delete str3.bat

--- str3.bat
+++
@@ -1,9 +0,0 @@
-rem
-rem  The alternative form of string comparison.  In this case,
-rem  the dot takes up space on the left side if no parameter
-rem  is given.
-rem
-
-if .%1==. goto end
-echo The first parameter is %1
-:end
